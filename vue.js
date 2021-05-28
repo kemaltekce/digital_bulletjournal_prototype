7fa4b63bd5d12b3d4664f7d99b10f3747473ec9f
Kemal Tekce <k.tekce@foodtracks.de>
2021-05-28 10:04:21 +0200

    add default bullet and add new page button

--- vue.js
+++ vue.js
@@ -48,6 +48,10 @@ var app = new Vue({
   },
   created() {
     this.page = this.pages[0]
+    this.addDefaultBullet()
+  },
+  updated() {
+    this.addDefaultBullet()
   },
   methods: {
     uuid() {
@@ -59,12 +63,38 @@ var app = new Vue({
       const isID = (element) => element.id === id
       this.page = this.pages[this.pages.findIndex(isID)]
     },
+    addNewPage() {
+      const newPage = {
+        id: this.uuid(),
+        title: 'Untitled',
+        collections: [
+          {
+            id: this.uuid(),
+            bullets: [
+              {id: this.uuid(), text: '', position: 0, style: 'empty'},
+            ]
+          }
+        ],
+      }
+      this.pages.push(newPage)
+    },
     editBulletText({collectionID, bulletID, newText}) {
       const isCollection = (element) => element.id === collectionID
       const isBullet = (element) => element.id === bulletID
       const collectionIndex = this.page.collections.findIndex(isCollection)
       const bulletIndex = this.page.collections[collectionIndex].bullets.findIndex(isBullet)
       this.$set(this.page.collections[collectionIndex].bullets[bulletIndex], 'text', newText)
+    },
+    addDefaultBullet() {
+      this.page.collections.forEach(collection => {
+        var lastBullet = collection.bullets[collection.bullets.length - 1]
+        if (lastBullet === undefined) {
+          collection.bullets.push({id: this.uuid(), text:"", position: 0, style: 'empty'})
+        } else if (lastBullet.text !== '') {
+          collection.bullets.push(
+            {id: this.uuid(), text:"", position: lastBullet.position + 1, style: 'empty'})
+        }
+      })
     }
   }
 })
